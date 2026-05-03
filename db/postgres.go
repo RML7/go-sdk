13001b85b0ece487df41bde9254d5e35c308e748
max <mraspisenko@bk.ru>
2026-05-03 14:49:56 +0300

    fix

--- db/postgres.go
+++ db/postgres.go
@@ -2,14 +2,18 @@ package db
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/RML7/go-sdk/closer"
+	"github.com/RML7/go-sdk/xerrors"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type DB struct {
 	*pgxpool.Pool
+}
+
+type Config struct {
+	DSN string `env:"DATABASE_DSN"`
 }
 
 // Executor возвращает DBExecutor из контекста (транзакция) или основной пул
@@ -21,19 +25,23 @@ func (db *DB) Executor(ctx context.Context) DBExecutor {
 	return db.Pool
 }
 
-func NewDB(ctx context.Context, dsn string) (*DB, error) {
-	cfg, err := pgxpool.ParseConfig(dsn)
-	if err != nil {
-		return nil, fmt.Errorf("parse dsn: %w", err)
+func NewDB(ctx context.Context, cfg Config) (*DB, error) {
+	if cfg.DSN == "" {
+		return nil, xerrors.Errorf("dsn is required")
 	}
 
-	pool, err := pgxpool.NewWithConfig(ctx, cfg)
+	poolCfg, err := pgxpool.ParseConfig(cfg.DSN)
 	if err != nil {
-		return nil, fmt.Errorf("create pool: %w", err)
+		return nil, xerrors.Errorf("parse dsn: %w", err)
+	}
+
+	pool, err := pgxpool.NewWithConfig(ctx, poolCfg)
+	if err != nil {
+		return nil, xerrors.Errorf("create pool: %w", err)
 	}
 
 	if err = pool.Ping(ctx); err != nil {
-		return nil, fmt.Errorf("ping db: %w", err)
+		return nil, xerrors.Errorf("ping db: %w", err)
 	}
 
 	closer.AddFunc(func(ctx context.Context) error {
